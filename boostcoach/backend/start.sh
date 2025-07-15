72c308b8e37babca29ab555765e13911847e4d7a
boostcoach <ceo@winnmedia.co.kr>
2025-07-16 01:14:34 +0900

    Remove prisma from Dockerfile and start.sh

--- boostcoach/backend/start.sh
+++ boostcoach/backend/start.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-export DATABASE_URL="postgresql://postgres:yzrhKiriXKrSYAdxIdPSjNuNseQkhjAe@postgres.railway.internal:5432/railway"
-
-# Run Prisma Migrations
-echo "Running Prisma Migrations..."
-PRISMA_CLI_BINARY_TARGETS=debian-openssl-1.1.x prisma migrate deploy
-
-# Generate Prisma Client
-echo "Generating Prisma Client..."
-PRISMA_CLI_BINARY_TARGETS=debian-openssl-1.1.x PRISMA_CLIENT_ENGINE_TYPE=binary prisma generate --no-engine || { echo "Error: Prisma Client generation failed!"; exit 1; }
-
 # Start Uvicorn server
 echo "Starting Uvicorn server..."
 uvicorn main:app --host 0.0.0.0 --port 3000
